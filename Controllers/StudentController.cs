a1a0eb7ee916aaccf1d000141be8320f55f85cc6
santuparsi <santuparsi@gmail.com>
2026-02-09 21:30:15 +0530

    commit_5

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public StudentController() { }
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "student1", "student2" };
+        }
+    }
+}
