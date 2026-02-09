74c3f46b44450d4b45c7161f17b19f566b0fdf4e
santuparsi <santuparsi@gmail.com>
2026-02-09 11:14:59 +0530

    commit_3

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -7,5 +7,10 @@ namespace SampleAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
     }
 }
